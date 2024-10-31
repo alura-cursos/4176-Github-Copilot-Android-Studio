197e08872f44a7a8083ccef37acf3d8c45526f11
Junior <35709152+git-jr@users.noreply.github.com>
2024-10-31 18:41:46 -0300

    aula 5.1

--- app/src/main/java/com/alura/anotaai/extensions/LongExtensions.kt
+++ app/src/main/java/com/alura/anotaai/extensions/LongExtensions.kt
@@ -1,14 +1,10 @@
 package com.alura.anotaai.extensions
 
-import java.util.Calendar
+import java.text.SimpleDateFormat
+import java.util.Locale
 
 fun Long.toDisplayDate(): String {
     val date = this
-    val calendar = Calendar.getInstance().apply {
-        timeInMillis = date
-    }
-    val day = calendar.get(Calendar.DAY_OF_MONTH)
-    val month = calendar.get(Calendar.MONTH) + 1
-    val year = calendar.get(Calendar.YEAR)
-    return "$day/$month/$year"
+    val formatter = SimpleDateFormat("dd/MM/yyyy HH:mm", Locale.getDefault())
+    return formatter.format(date)
 }
